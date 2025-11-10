b44325281c33d36e0fabe951e38cab787e7e58bf
zoey <zoey.spessanha@zeetech.io>
2025-11-10 13:38:06 -0300

    chore: release 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Supabase.Realtime.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/supabase-community/realtime-ex"
 
   def project do
